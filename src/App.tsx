8dba7bc8199bde8e5f188788d6c7580a39667c25
Kefe05 <oghenekefe.okpare@miva.edu.ng>
2025-03-12 00:26:42 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button";
+
+
+function App() {
+  return (
+    <>
+     <div className="h-screen w-screen bg-red-800">
+      
+     <Button variant="outline">Button</Button>
+
+     </div>
+    </>
+  );
+}
+
+export default App;
