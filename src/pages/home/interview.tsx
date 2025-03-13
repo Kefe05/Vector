a308f3374ec5c72938b7736f1457990dce52069b
Kefe05 <oghenekefe.okpare@miva.edu.ng>
2025-03-13 20:29:59 +0100

    removed interview list

--- src/pages/home/interview.tsx
+++ src/pages/home/interview.tsx
@@ -1,13 +1,9 @@
-import { DataTableDemo } from "./index";
+// import { DataTableDemo } from "./index";
 import dayjs from "dayjs";
 const Interview = () => {
   const date = dayjs("2019-01-25").format("DD/MM/YYYY");
   console.log(date);
-  return (
-    <div>
-      <DataTableDemo />
-    </div>
-  );
+  return <div>Hello Welcome to the Interview</div>;
 };
 
 export default Interview;
